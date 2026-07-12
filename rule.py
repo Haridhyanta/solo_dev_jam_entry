df97e9346576cfe6d09b858804fb58c8e7a968b7
Haridhyanta <haridhyantas@gmail.com>
2026-07-12 16:22:21 +0530

    added horizontal and vertical march rules

--- rule.py
+++ rule.py
@@ -667,6 +667,76 @@ class WMarch(Rule):
         for (x, y) in new_squares:
             grid[x, y] = selected_color
 
+class HMarch(Rule):
+    def __init__(
+            self,
+            name_font: pg.font.Font,
+            text_font: pg.font.Font,
+            name_color: pg.Color,
+            text_color: pg.Color,
+            outline_color: pg.Color,
+            outline_width: int,
+            bg_rect: pg.Rect,
+            bg_color: pg.Color,
+            option_size: tuple[int, int],
+        ) -> None:
+        name: str = "Horizontal March"
+        text: list[str] = ["The color", "spreads to the square directly to the left or right of it."]
+        super().__init__(name, text, name_font, text_font, name_color, text_color, outline_color, outline_width, bg_rect, bg_color, option_size)
+
+    def step(self, grid: ColorGrid) -> None:
+        selected_color = self.options[0]
+        if selected_color is None:
+            return
+        new_squares: list[tuple[int, int]] = []
+        for (x, y), color in grid.get_items():
+            if color != selected_color:
+                continue
+
+            if (x-1, y) in grid:
+                new_squares.append((x-1, y))
+
+            if (x+1, y) in grid:
+                new_squares.append((x+1, y))
+
+        for (x, y) in new_squares:
+            grid[x, y] = selected_color
+
+class VMarch(Rule):
+    def __init__(
+            self,
+            name_font: pg.font.Font,
+            text_font: pg.font.Font,
+            name_color: pg.Color,
+            text_color: pg.Color,
+            outline_color: pg.Color,
+            outline_width: int,
+            bg_rect: pg.Rect,
+            bg_color: pg.Color,
+            option_size: tuple[int, int],
+        ) -> None:
+        name: str = "Vertical March"
+        text: list[str] = ["The color", "spreads to the square directly to the left or right of it."]
+        super().__init__(name, text, name_font, text_font, name_color, text_color, outline_color, outline_width, bg_rect, bg_color, option_size)
+
+    def step(self, grid: ColorGrid) -> None:
+        selected_color = self.options[0]
+        if selected_color is None:
+            return
+        new_squares: list[tuple[int, int]] = []
+        for (x, y), color in grid.get_items():
+            if color != selected_color:
+                continue
+
+            if (x, y-1) in grid:
+                new_squares.append((x, y-1))
+
+            if (x, y+1) in grid:
+                new_squares.append((x, y+1))
+
+        for (x, y) in new_squares:
+            grid[x, y] = selected_color
+
 NAME_TO_RULE: dict[str, type] = {
     "S-E Spread": DRSpread,
     "N-E Cover": NECover,
@@ -682,4 +752,6 @@ NAME_TO_RULE: dict[str, type] = {
     "E March": EMarch,
     "S March": SMarch,
     "W March": WMarch,
+    "H March": HMarch,
+    "V March": VMarch,
 }
